495eae3b421ed4527a6251c81bd9e6ba2dabdd43
Cameron <cameron.trando@yahoo.com>
2017-12-18 22:18:07 -0800

    Hard coding URLS to improve speed

--- scrape.js
+++ scrape.js
@@ -14,11 +14,11 @@ function storeDHMenus(store_text, link, callback) {
 
   request(link, function (error, response, html) {
     let foodMeals = []
-    let meals = {
-      'Breakfast': false,
-      'Lunch': false,
-      'Dinner': false
-    }
+    let meals = [
+      'Breakfast',
+      'Lunch',
+      'Dinner'
+    ]
 
     if (!error && response.statusCode == 200) {
       let $ = cheerio.load(html);
@@ -36,24 +36,22 @@ function storeDHMenus(store_text, link, callback) {
       });
 
       // breakfast lunch or dinner 
-      $('td.restaurantTitle').each(function(k, meal) {
+   /*   $('td.restaurantTitle').each(function(k, meal) {
         let meal_text = $(meal).text();
         if(meals[meal_text] != undefined) {
           meals[meal_text] = true;
         }
-      });
+      }); */
 
 
       // idea here is that the meals and food meals should have the same number of items,
       // so we can map them together sequentially 
-      
+
       let food_count = 0;
 
-      for(let [meal, value] of Object.entries(meals)) {
-        console.log(meal + ' ' +  value);
-        if(value == false) continue;
-
+      meals.forEach(function(meal) {
         foodMeals[food_count].forEach(function(food) {
+          console.log(meal + ' ' +  food);
           if(food.indexOf('$') != -1) {
             db.insert(store_text, {
               'type': meal,
@@ -61,30 +59,27 @@ function storeDHMenus(store_text, link, callback) {
             });
           }
         });
-
         food_count++;
-      } 
+      });
     }
   });
 }
 
-function main(callback) {
-  //db.create_tables();
-  //db.drop_tables();
+function main() {
   request(start_website, function (error, response, html) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
 
       $('div#topnav.navigation ul li').children('a').each(function(i, store) {
-          let store_text = $(store).text();
-          let sep_store_text = store_text.split(' ');
-          let first_word = sep_store_text[0];
+        let store_text = $(store).text();
+        let sep_store_text = store_text.split(' ');
+        let first_word = sep_store_text[0];
 
-          if(db.is_valid_table(first_word)) {
-            let store_link = root_website+$(store).attr('href');
-            storeDHMenus(db.get_dining_hall(first_word), store_link, callback);
-          }                              
-        });
+        if(db.is_valid_table(first_word)) {
+          let store_link = root_website+$(store).attr('href');
+          storeDHMenus(db.get_dining_hall(first_word), store_link, callback);
+        }                              
+      });
     }
   });
 }
